ccf07673f7ea9ea6a4affd6e58cc61c34ad91150
Andy-TABs <l20550404@chihuahua2.tecnm.mx>
2024-03-12 21:17:29 -0600

    Create Ejemplo8.py

---
+++ Ejemplo8.py
@@ -0,0 +1,13 @@
+# Write code below 💖
+
+things_to_do = [
+  'Improve my hability with python',
+  'Learn to code Javascript',
+  'Make a videogame',
+  'Have more Cats',
+  'Live in another country for a few years'
+  ':)'
+]
+print('Some of the things I want to do in the future:')
+for i in range(len(things_to_do)):
+  print(f'{i +1}.-    {things_to_do[i]}')
